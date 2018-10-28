b2847e105503a5a0ad068f41f7b5dd48eec3f94d
Kirill <Kirill@Kirills-Laptop>
2018-10-28 23:39:03 +0100

    Finished the tutorial and changed the code so it fits the assignment

--- src/main/java/maas/Start.java
+++ src/main/java/maas/Start.java
@@ -2,21 +2,58 @@ package maas;
 
 import java.util.List;
 import java.util.Vector;
-//import maas.tutorials.BookBuyerAgent;
+import maas.tutorials.BookBuyerAgent;
+import maas.tutorials.BookSellerAgent;
+import maas.tutorials.Books;
 
 public class Start {
     public static void main(String[] args) {
-    	List<String> agents = new Vector<>();
-    	agents.add("tester:maas.tutorials.BookBuyerAgent");
-
+    	
+    	List<String> bookNames = Books.getBookList();
+    	
+    	// Change the buyer agents here
+    	final int numberOfBuyerAgents = 20;
+    	
+    	
+    	final int numberOfSellerAgents = 4;
+    	
+    	List<String> buyAgents = new Vector<>();
+    	for (int i = 0; i < numberOfBuyerAgents; ++i) {
+    		StringBuilder sb = new StringBuilder();
+    		sb.append("buyer");
+    		sb.append(i + 1);
+    		sb.append(":maas.tutorials.BookBuyerAgent");
+        	buyAgents.add(sb.toString());
+    	}
+    	
+    	List<String> sellAgents = new Vector<>();
+    	for (int i = 0; i < numberOfSellerAgents; ++i) {
+    		StringBuilder sb = new StringBuilder();
+    		sb.append("seller");
+    		sb.append(i + 1);
+    		sb.append(":maas.tutorials.BookSellerAgent");
+        	sellAgents.add(sb.toString());
+    	}
+    	
     	List<String> cmd = new Vector<>();
     	cmd.add("-agents");
     	StringBuilder sb = new StringBuilder();
-    	for (String a : agents) {
+    	int i = 0;
+    	for (String a : buyAgents) {
+    		sb.append(a);
+    		sb.append("(" + bookNames.get(i % bookNames.size()) + ");");
+    		++i;
+    	}
+    	
+    	i = 0;
+    	for (String a : sellAgents) {
     		sb.append(a);
     		sb.append(";");
+    		++i;
     	}
+    	
     	cmd.add(sb.toString());
+    	System.out.println(cmd);
         jade.Boot.main(cmd.toArray(new String[cmd.size()]));
     }
 }
--- src/main/java/maas/tutorials/BookBuyerAgent.java
+++ src/main/java/maas/tutorials/BookBuyerAgent.java
@@ -5,32 +5,76 @@ import jade.content.lang.sl.SLCodec;
 import jade.content.onto.basic.Action;
 import jade.core.Agent;
 import jade.core.behaviours.*;
+import jade.core.AID;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
 import jade.domain.FIPANames;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.JADEAgentManagement.JADEManagementOntology;
 import jade.domain.JADEAgentManagement.ShutdownPlatform;
 import jade.lang.acl.ACLMessage;
-
+import jade.lang.acl.MessageTemplate;
 
 @SuppressWarnings("serial")
 public class BookBuyerAgent extends Agent {
+
+	private String targetBookTitle;
+	private AID[] sellerAgents;
+	
 	protected void setup() {
-	// Printout a welcome message
-		System.out.println("Hello! Buyer-agent "+getAID().getName()+" is ready.");
+		// Printout a welcome message
+		System.out.println(getAID().getLocalName() + ": Hello! Buyer-agent  is ready.");
 
-        try {
- 			Thread.sleep(3000);
- 		} catch (InterruptedException e) {
- 			//e.printStackTrace();
- 		}
-		addBehaviour(new shutdown());
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// e.printStackTrace();
+		}
 
+		// Get the title of the book to buy as a start-up argument
+		Object[] args = getArguments();
+		if (args != null && args.length > 0) {
+			// Setting information
+			this.targetBookTitle = (String) args[0];
+			System.out.println(getAID().getLocalName() + ": Trying to buy " + targetBookTitle);
+
+			// Adding behavior
+			addBehaviour(new TickerBehaviour(this, 5000) {
+				protected void onTick() {
+					// Update the list of seller agents
+					DFAgentDescription template = new DFAgentDescription();
+					ServiceDescription sd = new ServiceDescription();
+					sd.setType("book-selling");
+					template.addServices(sd);
+					try {
+						DFAgentDescription[] result = DFService.search(myAgent, template);
+						sellerAgents = new AID[result.length];
+						for (int i = 0; i < result.length; ++i) {
+							sellerAgents[i] = result[i].getName();
+						}
+					} catch (FIPAException fe) {
+						fe.printStackTrace();
+					}
+					myAgent.addBehaviour(new RequestPerformer());
+				}
+			});
+		} else {
+			// Make the agent terminate immediately
+			System.out.println("No book title specified");
+			doDelete();
+		}
+
+//		addBehaviour(new shutdown());
 	}
+
 	protected void takeDown() {
 		System.out.println(getAID().getLocalName() + ": Terminating.");
 	}
 
-    // Taken from http://www.rickyvanrijn.nl/2017/08/29/how-to-shutdown-jade-agent-platform-programmatically/
-	private class shutdown extends OneShotBehaviour{
+	// Taken from
+	// http://www.rickyvanrijn.nl/2017/08/29/how-to-shutdown-jade-agent-platform-programmatically/
+	private class shutdown extends OneShotBehaviour {
 		public void action() {
 			ACLMessage shutdownMessage = new ACLMessage(ACLMessage.REQUEST);
 			Codec codec = new SLCodec();
@@ -40,13 +84,102 @@ public class BookBuyerAgent extends Agent {
 			shutdownMessage.setLanguage(FIPANames.ContentLanguage.FIPA_SL);
 			shutdownMessage.setOntology(JADEManagementOntology.getInstance().getName());
 			try {
-			    myAgent.getContentManager().fillContent(shutdownMessage,new Action(myAgent.getAID(), new ShutdownPlatform()));
-			    myAgent.send(shutdownMessage);
+				myAgent.getContentManager().fillContent(shutdownMessage,
+						new Action(myAgent.getAID(), new ShutdownPlatform()));
+				myAgent.send(shutdownMessage);
+			} catch (Exception e) {
+				// LOGGER.error(e);
 			}
-			catch (Exception e) {
-			    //LOGGER.error(e);
-			}
-
 		}
 	}
+	
+	
+	/**
+	 * Inner class RequestPerformer. This is the behaviour used by Book-buyer agents
+	 * to request seller agents the target book.
+	 */
+	private class RequestPerformer extends Behaviour {
+		private AID bestSeller; // The agent who provides the best offer
+		private int bestPrice; // The best offered price
+		private int repliesCnt = 0; // The counter of replies from seller agents
+		private MessageTemplate mt; // The template to receive replies
+		private int step = 0;
+
+		public void action() {
+			switch (step) {
+			case 0:
+				// Send the cfp to all sellers
+				ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+				for (int i = 0; i < sellerAgents.length; ++i) {
+					cfp.addReceiver(sellerAgents[i]);
+				}
+				cfp.setContent(targetBookTitle);
+				cfp.setConversationId("book-trade");
+				cfp.setReplyWith("cfp" + System.currentTimeMillis()); // Unique value
+				myAgent.send(cfp);
+				// Prepare the template to get proposals
+				mt = MessageTemplate.and(MessageTemplate.MatchConversationId("book-trade"),
+						MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+				step = 1;
+				break;
+			case 1:
+				// Receive all proposals/refusals from seller agents
+				ACLMessage reply = myAgent.receive(mt);
+				if (reply != null) {
+					// Reply received
+					if (reply.getPerformative() == ACLMessage.PROPOSE) {
+						// This is an offer
+						int price = Integer.parseInt(reply.getContent());
+						if (bestSeller == null || price < bestPrice) {
+							// This is the best offer at present
+							bestPrice = price;
+							bestSeller = reply.getSender();
+						}
+					}
+					repliesCnt++;
+					if (repliesCnt >= sellerAgents.length) {
+						// We received all replies
+						step = 2;
+					}
+				} else {
+
+					block();
+				}
+				break;
+			case 2:
+				// Send the purchase order to the seller that provided the best offer
+				ACLMessage order = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+				order.addReceiver(bestSeller);
+				order.setContent(targetBookTitle);
+				order.setConversationId("book-trade");
+				order.setReplyWith("order" + System.currentTimeMillis());
+				myAgent.send(order);
+				// Prepare the template to get the purchase order reply
+				mt = MessageTemplate.and(MessageTemplate.MatchConversationId("book-trade"),
+						MessageTemplate.MatchInReplyTo(order.getReplyWith()));
+				step = 3;
+				break;
+			case 3:
+				// Receive the purchase order reply
+				reply = myAgent.receive(mt);
+				if (reply != null) {
+					// Purchase order reply received
+					if (reply.getPerformative() == ACLMessage.INFORM) {
+						// Purchase successful. We can terminate
+						System.out.println(getAID().getLocalName() + ": "+ targetBookTitle + " successfully purchased. Price = " + bestPrice);
+						myAgent.doDelete();
+					}
+					step = 4;
+				} else {
+					block();
+				}
+				break;
+			}
+		}
+
+		public boolean done() {
+			return ((step == 2 && bestSeller == null) || step == 4);
+		}
+	} // End of inner class RequestPerformer
+
 }
